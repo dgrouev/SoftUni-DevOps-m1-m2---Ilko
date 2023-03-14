c4a0ccdf6d36349cd46374a9628e1be02961b567
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-14 14:26:31 +0200

    Daemon.json copied in /etc/docker during provisioning

--- m6/install_prometheus.sh
+++ m6/install_prometheus.sh
@@ -2,4 +2,10 @@
 
 echo "* Docker Swarm Init"
 docker swarm init --advertise-addr 192.168.99.101
+
+echo "* Add Vagrant user to Wheel Group"
 usermod -aG wheel vagrant
+
+echo "* Copying daemon.json to /etc/docker/"
+sudo cp /vagrant/daemon.json /etc/docker/daemon.json
+
