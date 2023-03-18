676da4524fcb87cf7ea538dd1d6427957fdd7729
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-18 15:46:02 +0200

    Metricbeat centos script created

---
+++ m7/metricbeat-centos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "* Downloading and Installing Metricbeat"
+wget https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.6.2-x86_64.rpm
+sudo rpm -Uvh metricbeat-8.6.2-x86_64.rpm
+
+echo "* Remove basic Metricbeat.yml"
+sudo sudo rm /etc/metricbeat/metricbeat.yml -f
+
+echo "* Copy configured Metricbeat.yml"
+sudo sudo cp /vagrant/metricbeat.yml /etc/metricbeat/metricbeat.yml
+
+echo "* Create Index Pattern via REST API"
+curl -XPOST http://192.168.99.101:5601/api/index_patterns/index_pattern -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
+{
+  "index_pattern": {
+    "name":"Metricbeat",
+    "title":"metricbeat-8.6.2-*",
+    "timeFieldName":"@timestamp"
+  }
+}'
+
+echo "* Start Metricbeat service"
+sudo systemctl daemon-reload
+sudo systemctl enable metricbeat
+sudo systemctl start metricbeat
