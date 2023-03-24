32e0ca627ec87f1e6760e064d6b6c65506bb40fe
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-24 22:26:28 +0200

    add_hosts.sh configured

--- exam/add_hosts.sh
+++ exam/add_hosts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "192.168.99.101 jenkins.do1.lab jenkins" >> /etc/hosts
-echo "192.168.99.102 docker.do1.lab docker" >> /etc/hosts
+echo "192.168.99.101 vm101.do1.exam vm101" >> /etc/hosts
+echo "192.168.99.102 vm102.do1.exam vm102" >> /etc/hosts
+echo "192.168.99.103 vm103.do1.exam vm103" >> /etc/hosts
