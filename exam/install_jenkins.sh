bdcf7742f01332cadf2393fa34db661f5d95e08f
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-24 22:48:49 +0200

    Slight refactor to the scripts

--- exam/install_jenkins.sh
+++ exam/install_jenkins.sh
@@ -5,7 +5,6 @@ wget https://pkg.jenkins.io/redhat/jenkins.repo -O /etc/yum.repos.d/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 
 echo "* Update repositories and install components"
-dnf update
 dnf install -y java-17-openjdk jenkins git
 
 echo "* Start the service"
