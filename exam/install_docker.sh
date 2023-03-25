5c5ed1d218a5e30956522615f85bb10529192551
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-25 14:13:51 +0200

    Node_exporter setup integrated with the jenkins and docker machine scripts

--- exam/install_docker.sh
+++ exam/install_docker.sh
@@ -14,4 +14,11 @@ sudo usermod -aG docker vagrant
 
 echo "* Adjust the firewall"
 firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --permanent --add-port=9100/tcp
 firewall-cmd --reload
+
+echo "* Setup node exporter"
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xzvf node_exporter-1.5.0.linux-amd64.tar.gz
+cd node_exporter-1.5.0.linux-amd64/
+./node_exporter &> /tmp/node-exporter.log &
