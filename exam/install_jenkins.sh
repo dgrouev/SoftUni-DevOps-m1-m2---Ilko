19f674eabc9f9001b7da388eb2c591a8b29dd96b
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-25 14:12:07 +0200

    Pipeline code and node exporter scripts added

--- exam/install_jenkins.sh
+++ exam/install_jenkins.sh
@@ -10,8 +10,15 @@ dnf install -y java-17-openjdk jenkins git
 echo "* Start the service"
 systemctl enable --now jenkins
 
+echo "* Setup node exporter"
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar xzvf node_exporter-1.5.0.linux-amd64.tar.gz
+cd node_exporter-1.5.0.linux-amd64/
+./node_exporter &> /tmp/node-exporter.log &
+
 echo "* Adjust the firewall"
 firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --permanent --add-port=9100/tcp
 firewall-cmd --reload
 
 echo "* admin password is:"
