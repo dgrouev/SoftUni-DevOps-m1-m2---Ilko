01fecfd79722bfade877104af14a21456e5c5666
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-25 18:49:45 +0200

    Jenkins user getting added to sudoers during provision

--- exam/install_jenkins.sh
+++ exam/install_jenkins.sh
@@ -10,6 +10,11 @@ dnf install -y java-17-openjdk jenkins git
 echo "* Start the service"
 systemctl enable --now jenkins
 
+echo "* Add Jenkins and adjust the group membership"
+echo -e 'Password1\nPassword1' | sudo passwd jenkins
+sudo usermod -s /bin/bash jenkins
+echo "jenkins  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
+
 echo "* Setup node exporter"
 wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
 tar xzvf node_exporter-1.5.0.linux-amd64.tar.gz
