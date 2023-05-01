422e39237d29b5a109664bddbcebc87df3fbc6a3
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-01 20:25:34 +0300

    Steps for adding Chef Organization added and firewall ports opening

--- do2/m4hw/bgapp/server_installation.sh
+++ do2/m4hw/bgapp/server_installation.sh
@@ -9,7 +9,7 @@ echo "Setting SELinux in permissive mode"
 sudo setenforce permissive
 sudo sed -i 's\=enforcing\=permissive\g' /etc/sysconfig/selinux
 
-echo "Installing Chef Workstation"
+echo "Installing Chef Server"
 wget -P /tmp https://packages.chef.io/files/stable/chef-server/15.6.2/el/8/chef-server-core-15.6.2-1.el8.x86_64.rpm
 sudo rpm -Uvh /tmp/chef-server-core-15.6.2-1.el8.x86_64.rpm
 sudo chef-server-ctl reconfigure --chef-license accept
--- do2/m4hw/bgapp/web_installation.sh
+++ do2/m4hw/bgapp/web_installation.sh
@@ -9,10 +9,16 @@ echo "Setting SELinux in permissive mode"
 sudo setenforce permissive
 sudo sed -i 's\=enforcing\=permissive\g' /etc/sysconfig/selinux
 
-echo "Installing Chef Workstation"
-wget -P /tmp https://packages.chef.io/files/stable/chef-server/15.6.2/el/8/chef-server-core-15.6.2-1.el8.x86_64.rpm
-sudo rpm -Uvh /tmp/chef-server-core-15.6.2-1.el8.x86_64.rpm
-sudo chef-server-ctl reconfigure --chef-license accept
+echo "Installing Chef Workstation and Git"
+wget -P /tmp https://packages.chef.io/files/stable/chef-workstation/23.4.1032/el/8/chef-workstation-23.4.1032-1.el8.x86_64.rpm
+sudo rpm -Uvh /tmp/chef-workstation-23.4.1032-1.el8.x86_64.rpm
+sudo dnf install -y git
 
+echo "Creating Chef Organization and Opening Firewall Ports 80 & 443"
+sudo chef-server-ctl user-create chefadmin Chef Admin chefadmin@do2.lab 'Password1' --filename /home/vagrant/chefadmin.pem
+sudo chef-server-ctl org-create demo-org 'Demo Org.' --association_user chefadmin --filename /home/vagrant/demoorg-validator.pem
+sudo firewall-cmd --add-port=80/tcp --permanent
+sudo firewall-cmd --add-port=443/tcp --permanent
+sudo firewall-cmd --reload
 
 
