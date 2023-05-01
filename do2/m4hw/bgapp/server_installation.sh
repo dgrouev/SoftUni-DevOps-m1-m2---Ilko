5698ad85a7305f7875eae481f1a9eec15e7b3aca
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-01 20:12:38 +0300

    Steps for auto-license accepting introduced upon server reconfigure

---
+++ do2/m4hw/bgapp/server_installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing and starting NTP client"
+sudo dnf install -y chrony
+sudo systemctl enable chronyd
+sudo systemctl start chronyd
+
+echo "Setting SELinux in permissive mode"
+sudo setenforce permissive
+sudo sed -i 's\=enforcing\=permissive\g' /etc/sysconfig/selinux
+
+echo "Installing Chef Workstation"
+wget -P /tmp https://packages.chef.io/files/stable/chef-server/15.6.2/el/8/chef-server-core-15.6.2-1.el8.x86_64.rpm
+sudo rpm -Uvh /tmp/chef-server-core-15.6.2-1.el8.x86_64.rpm
+sudo chef-server-ctl reconfigure --chef-license accept
+
+
+
