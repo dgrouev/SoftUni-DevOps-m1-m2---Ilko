fa14e309a37dd33b9f636b7f3c92979a7c898002
ilkoTheTiger <ilkothetiger@gmail.com>
2023-05-07 23:01:45 +0300

    Nginx manifest creates a nginx container properly

--- do2/m5hw/nginx/manifests/nginx.pp
+++ do2/m5hw/nginx/manifests/nginx.pp
@@ -3,7 +3,7 @@ class { 'docker': }
 docker::run { 'nginx':
   image            => 'nginx:latest',
   detach           => false,
-  ports            => '80',
+  ports            => ['80:80'],
 }
 
 class { 'firewall': }
@@ -12,11 +12,4 @@ firewall { '000 accept 80/tcp':
   action   => 'accept',
   dport    => 80,
   proto    => 'tcp',
-}
-
-selboolean { 'Apache SELinux':
-  name       => 'httpd_can_network_connect', 
-  persistent => true, 
-  provider   => getsetsebool, 
-  value      => on, 
 }
