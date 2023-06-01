6f440ac8ce6ff96c37897cd22079f10613cd927e
ilkoTheTiger <ilkothetiger@gmail.com>
2023-06-01 20:59:20 +0300

    Resource for starting Kafka container added

--- do2/exam-prep/tfm-1-kafka/main.tf
+++ do2/exam-prep/tfm-1-kafka/main.tf
@@ -13,3 +13,21 @@ resource "docker_image" "img-zookeeper" {
 resource "docker_image" "img-kafka" {
   name = "bitnami/kafka:latest"
 }
+
+resource "docker_container" "kafka" {
+  name = "kafka"
+  image = docker.image.img-kafka.image_id
+  env = ["KAFKA_BROKER_ID=1",
+         "KAFKA_CFG_ZOOKEEPER_CONNECT=zookeeper:2181",
+         "ALLOW_PLAINTEXT_LISTENER=yes"]
+  ports {
+    internal = 9092
+    external = 9092
+  }
+  network_advanced {
+    name = "exam-net"
+  }
+  depends_on = [ 
+    docker_image.img-zookeeper,
+   ]
+}
