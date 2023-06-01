e80258f7af3f56966ae2d91103019bc9b2fe271e
ilkoTheTiger <ilkothetiger@gmail.com>
2023-06-01 23:26:48 +0300

    Terraform file for starting Prometheus and Grafana containers added

--- do2/exam-prep/tfm-4-mon/main.tf
+++ do2/exam-prep/tfm-4-mon/main.tf
@@ -13,3 +13,37 @@ resource "docker_image" "img-prometheus" {
 resource "docker_image" "img-grafana" {
   name = "grafana/grafana"
 }
+
+resource "docker_container" "prometheus" {
+  name  = "prometheus"
+  image = docker_image.img-prometheus.image_id
+  ports {
+    internal = 9090
+    external = 9090
+  }
+  networks_advanced {
+    name = "exam-net"
+  }
+  volumes {
+    host_path      = "/vagrant/tfm-4-mon/prometheus.yml"
+    container_path = "/etc/prometheus/prometheus.yml"
+    read_only      = true
+  }
+}
+
+resource "docker_container" "grafana" {
+  name = "grafana"
+  image = docker_image.img-grafana.image_id
+  ports {
+    internal = 3000
+    external = 3000
+  }
+  networks_advanced {
+    name = "exam-net"
+  }
+  volumes {
+    host_path = "/vagrant/tfm-4-mon/datasource.yml"
+    container_path = "/etc/grafana/provisioning/datasource/datasource.yml"
+    read_only = true
+  }
+}
