ec23c5c431ce1c6ee73edcb1944dc8bdaeb35e28
ilkoTheTiger <ilkothetiger@gmail.com>
2023-06-02 23:08:23 +0300

    DB manifest added

---
+++ do2/exam-prep/manifests/db.pp
@@ -0,0 +1,48 @@
+$packages = [ 'git' ]
+
+package { $packages: }
+
+vcsrepo { '/code':
+  ensure => present,
+  provider => git,
+  source => 'https://github.com/shekeriev/do2-app-pack.git',
+}
+
+class { '::mysql::server':
+  root_password => '12345',
+  remove_default_accounts => true,
+  restart => true,
+  override_options => {
+    mysqld = { bind-address => '0.0.0.0'}
+  },
+}
+
+mysql::db { 'db1':
+  user => 'root',
+  password => '12345',
+  host => '%',
+  sql => ['/code/app1/db/db_setup.sql'],
+  enforce_sql => true,
+}
+
+mysql::db { 'db12':
+  user => 'root',
+  password => '12345',
+  host => '%',
+  sql => ['/code/app2/db/db_setup.sql'],
+  enforce_sql => true,
+}
+
+file_line { 'hosts-web':
+  ensure => present,
+  path => '/etc/hosts',
+  line => '192.168.99.101  web.do2.lab  web',
+  match => '^192.168.99.101',
+}
+
+file_line { 'hosts-db':
+  ensure => present,
+  path => '/etc/hosts',
+  line => '192.168.99.102  db.do2.lab  db',
+  match => '^192.168.99.102',
+}
